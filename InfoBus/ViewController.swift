28368e5b88172a75a28ddded2cda216f272a1078
roker <roker@rokers-iMac.local>
2018-04-13 16:24:49 -0700

    Initial Commit

---
+++ InfoBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfoBus
+//
+//  Created by roker on 4/13/18.
+//  Copyright © 2018 roker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
