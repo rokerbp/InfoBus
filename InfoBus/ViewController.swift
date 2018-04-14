8f80eca73e6218bc1fd7ad2c0d73ac9a1f61043a
roker <roker@rokers-iMac.local>
2018-04-13 18:18:17 -0700

    Implementado el pitch select para origen

--- InfoBus/ViewController.swift
+++ InfoBus/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        origenField.inputView = pickerView
+        origenField.textAlignment = .center
+        origenField.placeholder = "Seleccione su Origen"
+        
     }
 
+    @IBOutlet weak var origenField: UITextField!
+    
+    let origenes = ["Campanario", "Terraplaza", "Parque de la salud", "La estancia", "El Uvo", "Lomas de granada"]
+    var pickerView = UIPickerView()
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return origenes.count
+    }
 
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return origenes[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        origenField.text = origenes[row]
+        origenField.resignFirstResponder()
+    }
 }
 
