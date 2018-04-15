ff05f04e469e1e5a9e35b075daf33e5b0667c455
roker <roker@rokers-iMac.local>
2018-04-15 13:33:08 -0700

    Coregidos los selectores de origen y destino. - cuando se selecciona origen se despliega el picker de orígenes - cuando se selecciona destino se despliega el picked de destinos - cuando se tiene seleccionado orígenes y esta visible el picker orígenes y se selecciona el campo destinos, se oculta el picker origines y se muestra destinos, y viceversa.
    **Por revisa**
    - Cuando se ha seleccionado destino y se pulsa de nuevo en el campo destino volver a mostrar el picker destinos ( self.origenField.resignFirstResponder() ) permite hacerlo pero hay que revisar donde ponerlo para que no haya que pulsar afuera del selector para que desaparezca.
    - Implementar la lógica para que origen y destino no sean los mismos, se puede hacer con oridenField.text =! destinoField.text al pulsar el botón buscar ruta, además que los campos no puedan estar vacíos.

--- InfoBus/ViewController.swift
+++ InfoBus/ViewController.swift
@@ -8,72 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        origenField.inputView = pickerView
-        origenField.textAlignment = .center
-        origenField.placeholder = "Seleccione su Origen"
-        
-        pickerView2.delegate = self
-        pickerView2.dataSource = self
-        destinoField.inputView = pickerView2
-        destinoField.textAlignment = .center
-        destinoField.placeholder = "Seleccione su Destino"
-        
-    }
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
     @IBOutlet weak var origenField: UITextField!
+    let origenes = ["Campanario", "Terraplaza", "Parque de la salud", "La estancia", "El Uvo", "Lomas de granada"]
+    let destinos = ["CC Campanario", "Terraplaza", "Parque de la salud", "La estancia", "El Uvo", "Lomas de granada"]
+    
     @IBOutlet weak var destinoField: UITextField!
     
-    let origenes = ["Campanario", "Terraplaza", "Parque de la salud", "La estancia", "El Uvo", "Lomas de granada"]
-    var pickerView = UIPickerView()
-    var pickerView2 = UIPickerView()
+    @IBOutlet weak var origenSelector: UIPickerView!
+    
+    @IBOutlet weak var destinoSelector: UIPickerView!
+    
+    //funciones
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var countrows : Int = origenes.count
+            if pickerView == destinoSelector {
+                countrows = self.destinos.count
+            }
+            return countrows
+        }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == origenSelector {
+            let titleRow = origenes[row]
+                return titleRow
+        }
+        else if pickerView == destinoSelector {
+            let titleRow = destinos[row]
+                return titleRow
+        }
+        return ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == origenSelector {
+            self.origenField.text = self.origenes[row]
+            self.origenSelector.isHidden = true
+        }
+        
+        else if pickerView == destinoSelector {
+            self.destinoField.text = self.destinos[row]
+            self.destinoSelector.isHidden = true
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (textField == self.origenField && self.destinoSelector.isHidden == true){
+            self.origenSelector.isHidden = false
+        }
+        else if (textField == self.origenField && self.destinoSelector.isHidden == false){
+            self.destinoSelector.isHidden = true
+            self.origenSelector.isHidden = false
+        }
+        else if (textField == self.destinoField && self.origenSelector.isHidden == true){
+            self.destinoSelector.isHidden = false
+        }
+        else if (textField == self.destinoField && self.origenSelector.isHidden == false){
+            self.origenSelector.isHidden = true
+            self.destinoSelector.isHidden = false
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // ponemos leyendas en los textInput origen y destino
+        origenField.textAlignment = .center
+        origenField.placeholder = "Seleccione su origenl"
+        destinoField.textAlignment = .center
+        destinoField.placeholder = "Seleccione su destino"
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    //construir el picker para origen
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-        return origenes.count
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return origenes[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        origenField.text = origenes[row]
-        origenField.resignFirstResponder()
-        
-    }
-    
-    //construir el picker para el destino
-    public func numberOfComponents2(in pickerView2: UIPickerView) -> Int {
-        return 1
-    }
-    
-    public func pickerView2(_ pickerView2: UIPickerView, numberOfRowsInComponent component2: Int) -> Int{
-        return origenes.count
-    }
-    
-    func pickerView2(_ pickerView2: UIPickerView, titleForRow row: Int, forComponent component2: Int) -> String? {
-        return origenes[row]
-    }
-    
-    func pickerView2(_ pickerView2: UIPickerView, didSelectRow row: Int, inComponent component2: Int) {
-        destinoField.text = origenes[row]
-        destinoField.resignFirstResponder()
     }
     
     
